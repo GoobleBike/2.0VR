cf583bfe1a26cfc77640fbdcb37bf945ba49e547
Duilio Peroni <duilio.peroni@gmail.com>
2017-10-18 11:51:27 +0200

    Add files via upload

--- controller/test/misura_velocita/misura_velocita.ino
+++ controller/test/misura_velocita/misura_velocita.ino
@@ -1,17 +1,19 @@
 //The Gooble Bike 2.0VR!
-//Contatore di impulsi al secondo
+//Misura velocità
 //Basato su un Arduino UNO/Leonardo
 //Acquisisce il segnale di velocità del trainer
-//e determina il numero di impulsi in una base di 1 Sec.
+//e convertendo il numero di impulsi in una base di 1 Sec.
 //Segnali di I/O
 //PIN D2 INGRESSO: onda quadra (duty cycle 50%) dal trainer
 //Richiede la shield 485.
 //Diagnostica locale
 //PIN D9 USCITA: LED che replica il segnale di ingresso
+//PIN D5 USCITA: BRAKE forzato a 0
 
 //Definizione degli I/O
 #define SPEED    2
 #define SPEEDLED 9
+#define BRAKE    5
 
 //variabili globali
 volatile long startTBase;
@@ -23,6 +25,9 @@ void setup() {
   //configura I/O
   pinMode(SPEED,INPUT);
   pinMode(SPEEDLED, OUTPUT);
+  pinMode(BRAKE,OUTPUT);
+  analogWrite(BRAKE,LOW);
+  //avvia contatore ed interrupt
   startTBase=micros();
   counter=0;
   attachInterrupt(digitalPinToInterrupt(SPEED), rising, RISING);
@@ -42,9 +47,10 @@ void falling() {
 }
 
 void loop() {
+  //stampa e resetta il contatore ogni secondo
   if(micros()-startTBase> 1000000L) {
-    float velocita= 1.0/(float(counter));
-    Serial.print(counter,3);
+    int vel=map(counter,0,86,0,39);
+    Serial.print(vel);
     Serial.println(" km/h");
     startTBase=micros();
     counter=0;
