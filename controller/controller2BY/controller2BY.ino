737646f5ad737ac02812b08c7d3f07cfb9e5e8b5
Duilio Peroni <duilio.peroni@gmail.com>
2017-11-14 08:51:13 +0100

    controller 2BY: comunica su UDP, hw YUN

--- controller/controller2BY/controller2BY.ino
+++ controller/controller2BY/controller2BY.ino
@@ -1,20 +1,24 @@
 //The Gooble Bike 2.0VR!
-//Versione BY (Yun)
+//Versione BY
+//HW Arduino Yun
+//richiede lo script udp-client2BY.py in autoavvio sul lato Linino
 //Campiona la velocità con interrupt (senza filtri antirimbalzo)
 //Campiona il valore analogico dello sterzo
-//Trasmette i dati su UDP via Bridge
-//Riceve la pendenza da UDP via Bridge
+//Trasmette i dati su UDP via Bridge ogni 250 mS
+//Riceve la pendenza da UDP via Bridge ogni 250 mS
 //Attua la pendenza
 //Segnali di I/O
 //PIN D2 INGRESSO: onda quadra (duty cycle 50%) dal trainer
 //PIN A0 INGRESSO: angolazione dello sterzo valore analogico 0-1023
 //PIN D5 USCITA: intensità di frenata PWM 0-255
+//PIN D6 USCITA: switch di debug: logica negata 1 no debug (default), 0 debug
 //Richiede la shield 485.
 //Diagnostica locale
 //PIN D9 USCITA: LED che indica la velocità
 //PIN D3 USCITA: LED che indica la frenata
 //PIN D12 USCITA: LED che segnala la comunicazione
-//8/11/2017
+//Diagnostica su seriale se debug abilitato
+//14/11/2017
 #include <Bridge.h>
 #include <stdio.h>
 //Definizione degli I/O
@@ -24,6 +28,7 @@
 #define BRAKELED 9      //LED segnalazione freno
 #define COMLED  12      //LED segnalazione comunicazione
 #define POT     A0      //Potenziometro angolazione
+#define DEBUG    6      //pin debug 1=debug default, 0= no debug
 
 //definizioni costanti
 #define TBASE 1000000L  //time base 1 secondo per calcolo velocità
@@ -41,13 +46,20 @@ int bkspeed;                  //misura ingresso velocità
 int angle;                  //misura ingresso angolazione
 int pend;                   //valore pendenza in %
 int brake;                  //valore uscita di frenata
-int rxStatus;               //stato automa ricezione
-int rxValue;                //valore ricevuto 
+int debug;                  //stato di debug
+char txBuf[]="00 +000";     //buffer di trasmissione
+int  txBufLen=sizeof(txBuf);//lunghezza del buffer di tx
+char rxBuf[]="00";          //buffer di ricezione
+int  rxBufLen=sizeof(txBuf);//lunghezza del buffer di rx
+int  nRxChar;               //numero caratteri letti
 
 void setup() {
-  //apre console 
+  //apre canali di console  e di bridge
   Serial.begin(250000);
+  delay(10);
   Bridge.begin();
+  delay(10);
+  
   //configura I/O
   pinMode(BKSPEED,INPUT);
   pinMode(SPEEDLED, OUTPUT);
@@ -58,16 +70,27 @@ void setup() {
   digitalWrite(BRAKELED,LOW);
   pinMode(COMLED,OUTPUT);
   digitalWrite(COMLED,LOW);
+  pinMode(DEBUG,INPUT_PULLUP);
+  
   //inizializza variabili di stato
   bkspeed=0;
   angle=0;
   pend=0;
   brake=0;
-  rxStatus=WT_1ST_CH;
-  rxValue=0;
+  
+  //imposta debug (logica negativa 1=no, 0=si)
+  debug=!digitalRead(DEBUG);
+  //TEMPORANEO!!!
+  debug=1;
+  //FINE TEMPORANEO!!!
+  if(debug){
+    Serial.println("Controller 2BY Debug mode");
+  }
+  
   //inizializza timers
   startTSend=micros();
   startTBase=micros();
+  
   //avvia contatore ed interrupt
   counter=0;
   attachInterrupt(digitalPinToInterrupt(BKSPEED), rising, RISING);
@@ -105,21 +128,27 @@ void loop() {
   if(micros()-startTSend>= TSEND) {   //verifica se time base 250 mS scaduto
     startTSend=micros();              //riavvia timebase
     digitalWrite(COMLED,HIGH);        //diagnostica on
-    char txBuf[10];
-    sprintf(txBuf, "%02d %+04d", bkspeed,angle);
+    //invio
+    snprintf(txBuf,txBufLen, "%02d %+04d", bkspeed,angle);
     Bridge.put("spdang",String(txBuf));
-    char rxBuf[10];
-    rxBuf[0]="0";
-    rxBuf[1]="0";
-    rxBuf[2]=0;
-    int nchar=Bridge.get("slope",rxBuf,10);
-    //pend=atoi(rxBuf);
-    if((rxBuf[0]>="0")&&(rxBuf[0]<="9")){
-      pend=(rxBuf[0] & 0x0F)*10;
+    if(debug){
+      Serial.println(txBuf);
     }
-    if((rxBuf[1]>="0")&&(rxBuf[1]<="9")){
-      pend=pend+ (rxBuf[1] & 0x0F);
+    //ricezione
+    nRxChar=Bridge.get("slope",rxBuf,rxBufLen);
+    if(debug){
+      Serial.print(nRxChar);
+      Serial.print(" ");
+      Serial.println(rxBuf);
     }
+    if(nRxChar>0){
+      if((rxBuf[0]>='0')&&(rxBuf[0]<='9')){
+        pend=(rxBuf[0] & 0x0F)*10;
+      }
+      if((rxBuf[1]>='0')&&(rxBuf[1]<='9')){
+        pend=pend+ (rxBuf[1] & 0x0F);
+      }
+    }  
     digitalWrite(COMLED,LOW);         //diagnostica off
   }
   
