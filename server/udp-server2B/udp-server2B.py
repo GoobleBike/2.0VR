bde25e3de3261ad19e7a2c76889b06250f53ceca
Duilio Peroni <duilio.peroni@gmail.com>
2017-11-22 12:35:47 +0100

    tolta forzatura di debug

--- server/udp-server2B/udp-server2B.py
+++ server/udp-server2B/udp-server2B.py
@@ -17,7 +17,7 @@ atexit.register(closeSock)
 #sys.argv[2]: <localport>
 #sys.argv[3]: True = debug, False no debug
 #TEMPORANEO: simulazione parametri per debug in Python IDE RIMUOVERE!!!
-sys.argv = [sys.argv[0], "0.0.0.0",40000,True]
+#sys.argv = [sys.argv[0], "0.0.0.0",40000,True]
 #FINE TEMPORANEO!!!
 if len(sys.argv)<4:
     print "parametri mancanti"
